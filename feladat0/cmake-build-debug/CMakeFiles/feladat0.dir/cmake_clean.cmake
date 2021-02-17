569af02e3d5f1a707be2080d270ddc3dd4e3be88
PalSzilard <palszili@outlook.com>
2021-02-17 11:25:50 +0200

    first

---
+++ feladat0/cmake-build-debug/CMakeFiles/feladat0.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/feladat0.dir/main.c.obj"
+  "feladat0.exe"
+  "feladat0.exe.manifest"
+  "feladat0.pdb"
+  "libfeladat0.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/feladat0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
